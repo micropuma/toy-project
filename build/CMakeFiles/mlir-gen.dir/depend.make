6d538ef7b6f9bc11f514de4015f0224965c0486a
micropuma <micropuma@163.com>
2024-07-25 19:54:56 +0800

    change ignore, some mistakes happend and have been fixed now

--- build/CMakeFiles/mlir-gen.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for mlir-gen.
-# This may be replaced when dependencies are built.
--- build/toy/lib/CMakeFiles/Toy.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libToy.a"
-  "libToy.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/Toy.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
