c24fd230b17428a4305d6633ba78db7b2861b5a3
micropuma <micropuma@163.com>
2024-08-03 12:37:56 +0800

    change test script

--- toy.sh
+++ toy.sh
@@ -2,3 +2,4 @@ build/bin/toy-opt --help
 build/bin/toy-opt ./test/right/basic.mlir
 build/bin/toy-opt -canonicalize ./test/right/cse.mlir
 build/bin/toy-opt -cse ./test/right/cse.mlir
+build/bin/toy-opt ./test/right/func.mlir
